da844ebf3678a9860114ed40d62427a1086f59ed
Francisca <francisca.paredes.pr@gmail.com>
2025-02-22 14:07:27 -0300

    class Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """Un tema sobre el que està aprendiendo el usuario."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devuelve una representaciòn del modelo como cadena."""
+        return self.text
