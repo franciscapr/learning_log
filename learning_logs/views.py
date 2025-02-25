d4e2a01edddf99859862c4145cef77d23ffd8b05
Francisca <francisca.paredes.pr@gmail.com>
2025-02-25 12:02:02 -0300

    def index

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """La pàgina de incio para Learning log."""
+    return render(request, 'learning_logs/index.html')
