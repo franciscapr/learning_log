18c291beb6055c9ef790f60b24a92ff572ffd877
Francisca <francisca.paredes.pr@gmail.com>
2025-02-26 11:23:22 -0300

    url topic_id

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Pàgina que muestra todos los temas.
     path('topics/', views.topics, name='topics'),
+    # Pàgina de detalle sobre un tema individual.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
