e1d28ca6231786d4c2f2d72f7a627b06c6186b01
Francisca <francisca.paredes.pr@gmail.com>
2025-02-26 12:20:36 -0300

    topic.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 
 # Create your views here.
 def index(request):
@@ -8,9 +8,17 @@ def index(request):
 
 
 # La vista de topics
-def topics(request, topic_id):
+
+def topics(request):
+    """Muestra todos los temas."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
     """Muestra todos los temas y todas sus entradas."""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topics.html', context)
+    return render(request, 'learning_logs/topic.html', context)
