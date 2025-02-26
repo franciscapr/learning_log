4682dfa8afcab10c8dad068d6bd632f328af00e2
Francisca <francisca.paredes.pr@gmail.com>
2025-02-26 11:40:56 -0300

    template topic.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,8 +8,9 @@ def index(request):
 
 
 # La vista de topics
-def topics(request):
-    """Muestra todos los temas."""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
+def topics(request, topic_id):
+    """Muestra todos los temas y todas sus entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topics.html', context)
