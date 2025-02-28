cb0d1b17f5e281c924bfb8b8edbc9d8c0bb4994d
Francisca <francisca.paredes.pr@gmail.com>
2025-02-28 14:25:38 -0300

    urls.py register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 """ Define patrones de URL para accounts. """
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    # Incluye url de autneticaciòn predeterminadas.
+    # Incluye url autenticadas predeterminadas.
     path('', include('django.contrib.auth.urls')),
+    # Pàgina de registro.
+    path('register/', views.register, name='register'),
 ]
