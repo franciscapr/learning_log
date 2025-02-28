0f38001e77227680bdbe74c3f115c7e85f62164e
Francisca <francisca.paredes.pr@gmail.com>
2025-02-28 14:31:47 -0300

    view register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Registra un nuevo usuario."""
+    if request.method != 'POST':
+    # Muestra un formulario de registro en blanco.
+        form = UserCreationForm()
+    else:
+        # Procesa un formulario cumplimentado.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Inicia la sesiòn del usuario y lo redirige a la pàgina de inicio.
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    # Muestra una formulario en blanco o no vàlido.
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
